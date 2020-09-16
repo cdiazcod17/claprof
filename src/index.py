4ed0147434f5ec6e6bbd6ea53df078e6865d9edb
cdiazcod17 <cdiazcoder@gmail.com>
2020-09-16 10:00:37 -0500

    Updated

--- src/index.py
+++ src/index.py
@@ -4,7 +4,11 @@ from flask_mysqldb import MySQL
 #creo objeto para crear rutas
 app = Flask(__name__)
 #conexion a la base de datos
-mysql = MySQL()
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'claprofdb'
+mysql = MySQL(app)
 
 #crear rutaS
 @app.route('/')
@@ -18,6 +22,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 #comprobar si estamos en el archivo principal
 if __name__ == '__main__':
